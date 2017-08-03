871c7d650292eb4c390b676bef97309a4762dbef
ludwig <ludwig@10.211.55.2>
2017-08-03 16:54:31 -0700

    Added test for collision with global javascript variables.

--- samples/ParameterPassing/ParameterPassing.js
+++ samples/ParameterPassing/ParameterPassing.js
@@ -1,6 +1,8 @@
+c = ": " //If this is b, an error will be generated
+
 function Say(msg) {
 
-	a = "Say: ";
+	a = "Say" + c;
 	print(a + msg);
 	com.stottlerhenke.simbionic.test.engine.TestEngine.buffer.append(msg);
 }
