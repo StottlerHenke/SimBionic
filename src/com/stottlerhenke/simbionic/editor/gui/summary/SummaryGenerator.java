51cd00feb8342b26b92209427f18fb11ecd53704
Emilio Remolina <remolina@stottlerhenke.com>
2018-04-19 18:58:10 -0700

    prompt for output directory, copy simbionic.xsl to output directory, etc

--- src/com/stottlerhenke/simbionic/editor/gui/summary/SummaryGenerator.java
+++ src/com/stottlerhenke/simbionic/editor/gui/summary/SummaryGenerator.java
@@ -7,7 +7,10 @@ import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -15,14 +18,16 @@ import java.util.Vector;
 
 import javax.imageio.ImageIO;
 import javax.imageio.ImageWriter;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-import javax.swing.JViewport;
+import javax.swing.JOptionPane;
 
+import com.stottlerhenke.simbionic.common.xmlConverters.XMLObjectConverter;
 import com.stottlerhenke.simbionic.common.xmlConverters.model.Behavior;
 import com.stottlerhenke.simbionic.common.xmlConverters.model.BehaviorFolder;
 import com.stottlerhenke.simbionic.common.xmlConverters.model.BehaviorFolderGroup;
 import com.stottlerhenke.simbionic.common.xmlConverters.model.SimBionicJava;
+import com.stottlerhenke.simbionic.editor.FileManager;
 import com.stottlerhenke.simbionic.editor.SB_Behavior;
 import com.stottlerhenke.simbionic.editor.SimBionicEditor;
 import com.stottlerhenke.simbionic.editor.gui.SB_Drawable;
@@ -31,29 +36,96 @@ import com.stottlerhenke.simbionic.editor.gui.SB_Element;
 import com.stottlerhenke.simbionic.editor.gui.SB_Polymorphism;
 
 /**
+ * Create images of behaviors used to generate a project summary
+ * via xslt.
  * 
  * @author remolina
  *
  */
 public class SummaryGenerator {
 	protected SimBionicEditor _editor;
-	 
+	static File directoryOfLastOpenedFile= null; 
+	
+	/**
+	 * Create images of behaviors used to generate a project summary
+	 * via xslt.
+	 * @param editor
+	 */
 	public SummaryGenerator ( SimBionicEditor editor) {
 		this._editor = editor;
 	}
 	
-	public void generate () {
+	public void generate() {
+		JFileChooser fc = 
+			new JFileChooser(directoryOfLastOpenedFile == null ? System.getProperty("user.dir") : directoryOfLastOpenedFile.getAbsolutePath()) ;
+		
+		fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		fc.setDialogTitle("Select Output Directory");
+		if (fc.showSaveDialog(null) == JFileChooser.APPROVE_OPTION){
+			File outputDirectory = fc.getSelectedFile();
+			try{
+				generate(outputDirectory);
+				directoryOfLastOpenedFile = outputDirectory;
+				JOptionPane.showMessageDialog(null, "Images created at \n" +outputDirectory.getAbsolutePath(), "Output Created", JOptionPane.INFORMATION_MESSAGE);
+			}
+			catch (Exception ex){
+				ex.printStackTrace();
+				JOptionPane.showMessageDialog(null, ex + "\n" + ex.getMessage(), "Save Error", JOptionPane.ERROR_MESSAGE);
+			}
+		}
+	}
+	
+	/**
+	 * Save project xml and behavior images to the given directory
+	 * @param outputDirectory
+	 * @throws IOException 
+	 */
+	public void generate (File outputDirectory) throws IOException {
+		setupOutputDirectory(outputDirectory);
 		SimBionicJava model = _editor.getProjectBar().getDataModel();
 		List<SB_Behavior> behaviors = new ArrayList<>();
 		collectBehaviors(model.getBehaviors(),behaviors);
 		
 		for (SB_Behavior behavior : behaviors) {
-			System.out.println(behavior.getName());
+			//System.out.println(behavior.getName());
 			try {
-				takeImage(behavior);
+				takeImage(behavior,outputDirectory);
 			}
 			catch (Exception e) {
 				e.printStackTrace();
+			}
+		}
+		
+		try {
+			File projectFile = _editor.getProjectBar()._projectFile;
+			String projectName = (projectFile==null) ? "sb_project" : projectFile.getName();
+			int extensionIndex = projectName.indexOf(".");
+			if (extensionIndex >-1) {
+				projectName = projectName.substring(0,extensionIndex);
+			}
+			File outputFile = new File(outputDirectory,projectName+".xml");
+			XMLObjectConverter.getInstance().saveXML(model, outputFile);
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		} 
+	}
+	
+	/**
+	 * Create directory if necessary, remove any images in the directory
+	 * @param outputDirectory
+	 * @throws IOException 
+	 */
+	protected void setupOutputDirectory(File outputDirectory) throws IOException {
+		if (outputDirectory.isDirectory()) {
+			outputDirectory.mkdirs();
+			//delete all png files from the directory
+			
+			//copy simbionic.xls to output directory
+			File in = new File("xsl/simbionic.xsl");
+			File out = new File(outputDirectory,"simbionic.xsl");
+			if (!out.exists()) {
+				Files.copy(in.toPath(),out.toPath());
 			}
 		}
 	}
@@ -78,33 +150,24 @@ public class SummaryGenerator {
 	/**
 	 * Create images for the given behavior including its polymorphism
 	 * @param behavior
+	 * @param outputDirectory directory where behavior images are places
 	 * @throws Exception
 	 */
-    public void takeImage(SB_Behavior behavior) throws Exception {
+    public void takeImage(SB_Behavior behavior, File outputDirectory) throws Exception {
         SB_Polymorphism poly;
         Simple_Canvas canvas;
-        JViewport viewport;
-        JScrollPane scrollCanvas;
         int polyCount = behavior.getPolyCount();
  
         for (int i = 0; i < polyCount; ++i) {
             poly = behavior.getPoly(i);
-      
-            canvas = new Simple_Canvas();
-            scrollCanvas = new JScrollPane(canvas);
-            scrollCanvas.getHorizontalScrollBar().setUnitIncrement(10);
-            scrollCanvas.getVerticalScrollBar().setUnitIncrement(10);
-            //canvas.setNeedToScroll(true); // note: must call// scrollRectToVisible() later...
-            
+            canvas = new Simple_Canvas();            
             canvas.setPoly(poly);
-            //canvas.scrollRectToVisible(canvas.getPoly()._lastViewRect); // ...because
-            scrollCanvas.setAutoscrolls(true);
-            takeImage(canvas,behavior,poly);
+            takeImage(canvas,behavior,poly,i+1,outputDirectory);
         }
 
     }
     
-    protected void takeImage(Simple_Canvas canvas,SB_Behavior behavior,  SB_Polymorphism poly) throws Exception {
+    protected void takeImage(Simple_Canvas canvas,SB_Behavior behavior,  SB_Polymorphism poly, int polyPosition, File outputDirectory) throws Exception {
     	updateCanvas(canvas);
     	JFrame f = new JFrame();
 		f.getContentPane().setLayout(new BorderLayout());
@@ -126,9 +189,8 @@ public class SummaryGenerator {
 		canvas.repaint();
 		
 		
-		String fileName = "images/"+behavior.getName() + "_" + poly.getIndicesLabel()+".png";
+		File fileName = new File(outputDirectory,"btn_"+behavior.getName() + "_" + polyPosition+".png");
 		createImageIO(canvas,fileName,"png");
-    	 
     }
     
     protected void updateCanvas(Simple_Canvas canvas) {
@@ -148,7 +210,7 @@ public class SummaryGenerator {
     	Rectangle bounds = null;
     	for (SB_Drawable drawable : drawables) {
     		Rectangle dPosition = position(drawable);
-    		System.out.println(dPosition);
+    		//System.out.println(dPosition);
     		bounds = union(bounds,position(drawable));
     	}
     	return bounds;
@@ -178,7 +240,7 @@ public class SummaryGenerator {
     	Rectangle bounds = null;
     	for (SB_Drawable drawable : drawables) {
     		Rectangle dPosition = position(drawable);
-    		System.out.println(dPosition);
+    		//System.out.println(dPosition);
     		bounds = union(bounds,position(drawable));
     	}
     	return bounds;
@@ -189,10 +251,10 @@ public class SummaryGenerator {
 	 * The format file should be one returned by {@link ImageIO#getWriterFormatNames()}.
 	 * 
 	 * @param file
-	 * @param formatName  a String containg the informal name of the format. See {@link ImageIO#getWriterFormatNames()}
+	 * @param formatName  a String containing the informal name of the format. See {@link ImageIO#getWriterFormatNames()}
 	 * @throws Exception
 	 */
-	public void createImageIO(Simple_Canvas canvas,String file, String formatName) throws Exception {
+	public void createImageIO(Simple_Canvas canvas, File file, String formatName) throws Exception {
 		Iterator<ImageWriter> availableWriters = ImageIO.getImageWritersByFormatName(formatName);
 		if (availableWriters == null || !availableWriters.hasNext()) {
 			throw new RuntimeException("There is not a Java writer to export to " + formatName );
@@ -207,7 +269,7 @@ public class SummaryGenerator {
 		bounds.x -=100;
 		bounds.width +=200;
 		
-		System.out.println(bounds);
+		//System.out.println(bounds);
 		Dimension size = new Dimension(bounds.width,bounds.height);
 		int margin = 0;
 		//  	Dimension size = this.getPreferredSize();
