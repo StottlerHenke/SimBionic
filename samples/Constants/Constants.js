193594fb61a051c6d0251b1788edcbc7b3df16cd
Christopher Gioia <cgioia@stottlerhenke.com>
2018-04-20 09:33:23 -0700

    added simbionic test for constants

---
+++ samples/Constants/Constants.js
@@ -0,0 +1,14 @@
+
+// Say function
+function Say(message) {
+   print("Say function in HelloWorld.js " + message);
+}
+
+
+function addNumbersJs(a, b) {
+   return a + b;
+}
+
+function addNumbersJavaThroughJs(a, b) {
+	return ConstantsTest.addNumbersJavaThroughJs(a, b);
+}
