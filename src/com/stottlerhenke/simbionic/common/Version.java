86495fb4c752fe4265ccda321f1e3f20ffed6e82
jmingmao <jmingmao@stottlerhenke.com>
2018-06-01 14:08:02 -0700

    Updated Version Number (Primarily for UI changes)

--- src/com/stottlerhenke/simbionic/common/Version.java
+++ src/com/stottlerhenke/simbionic/common/Version.java
@@ -11,13 +11,13 @@ public class Version
 	public final static String SIMBIONIC_VERSION;
 
 	static {
-		SIMBIONIC_VERSION = "3.0.13"; 
+		SIMBIONIC_VERSION = "3.0.15"; 
 	}
 
 	public final static String SIMBIONIC_BUILD_DATE;
 	
 	static {
-		SIMBIONIC_BUILD_DATE = "2018-04-11";
+		SIMBIONIC_BUILD_DATE = "2018-06-01";
 	}
 	
 	
